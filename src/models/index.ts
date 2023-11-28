07b921f0fbbd1612c588f16445b9181ea0d6ac3e
Amanda Portela <mandaptela@gmail.com>
2023-11-28 17:26:28 -0300

    ajuste tsconfig.json

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,14 +1,5 @@
 import { Sequelize } from 'sequelize';
 import * as config from '../config/config.ts';
-import { type } from 'os';
-
-type config = {
-    username: string;
-    password: string;
-    database: string;
-    host: string;
-    dialect: string;
-}
 
 const sequelize = new Sequelize(config)
 
