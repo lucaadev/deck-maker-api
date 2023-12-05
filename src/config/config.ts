d0b140ffb67bb97277c452e219bc7b4eca5811fa
Amanda Portela <mandaptela@gmail.com>
2023-12-05 18:46:07 -0300

    fix - ajuste configurações sequelize

---
+++ src/config/config.ts
@@ -0,0 +1,10 @@
+import 'dotenv/config';
+import { Options } from 'sequelize';
+
+export const config: Options = {
+  dialect: "mysql",
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
+};
