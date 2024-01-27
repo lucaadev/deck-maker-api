4a9969c28cd93971684bfcd8c902712a422e18ac
Amanda Portela <mandaptela@gmail.com>
2024-01-27 18:07:33 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,11 @@
 import { App } from './App';
 import 'dotenv/config';
+declare var process : {
+    env: {
+      PORT: string
+    }
+  }
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT;
 
 new App().start(PORT);
